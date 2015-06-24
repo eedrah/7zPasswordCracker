ac865539b5080c53110af0d06104797079874ee6
Eedrah <eedrah@users.noreply.github.com>
2015-06-24 22:47:46 +1200

    test Permute returns all words given

--- UnitTestProject1/TestPermutator.cs
+++ UnitTestProject1/TestPermutator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using _7zPasswordCracker;
 
@@ -12,6 +13,20 @@ namespace Tests {
             IEnumerable<string> results = permutator.Permutate(words);
 
             Assert.Contains("word", results);
+        }
+
+        [Fact]
+        public void Should_Return_All_Words_Given()
+        {
+            var permutator = new Permutator();
+            var words = new List<string> { "some", "words", "given"};
+
+            List<string> results = permutator.Permutate(words).ToList();
+
+            foreach (string word in words)
+            {
+                Assert.Contains(word, results);
+            }
         }
     }
 }
