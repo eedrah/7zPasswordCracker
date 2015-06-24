66b3e93b75dad5b81f12f869bb87156f43b1a807
Eedrah <eedrah@users.noreply.github.com>
2015-06-24 22:43:24 +1200

    test permute returns the input word

---
+++ 7zPasswordCracker/Permutator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7zPasswordCracker {
+    public class Permutator {
+        public IEnumerable<string> Permutate(List<string> words)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestProject1/TestPermutator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using _7zPasswordCracker;
+
+namespace Tests {
+    public class TestPermutator {
+        [Fact]
+        public void Should_Return_The_Word_That_Was_Given() {
+            var permutator = new Permutator();
+            var words = new List<string> { "word" };
+
+            IEnumerable<string> results = permutator.Permutate(words);
+
+            Assert.Contains("word", results);
+        }
+    }
+}
