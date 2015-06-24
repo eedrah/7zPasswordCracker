b24a568fb54321977efb30806614cd40c14b6b18
Eedrah <eedrah@users.noreply.github.com>
2015-06-24 23:27:13 +1200

    test Orderer Should_Return_IEnumerables_Of_All_Numbers_From_0_To_The_Input

---
+++ 7zPasswordCracker/Orderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7zPasswordCracker {
+    public class Orderer {
+        public IEnumerable<IEnumerable<int>> GetOrderings(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestProject1/TestOrderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using _7zPasswordCracker;
+
+namespace Tests {
+    public class TestOrderer {
+        [Fact]
+        public void Should_Return_IEnumerables_Of_All_Numbers_From_0_To_The_Input() {
+            var orderer = new Orderer();
+            int input = 3;
+
+            List<IEnumerable<int>> orderings = orderer.GetOrderings(input).ToList();
+
+            for (int i = 1; i < 3; i++)
+            {
+                int number = i;
+                Assert.Contains(orderings, ints => ints.Single() == number);
+            }
+        }
+    }
+}
