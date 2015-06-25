939874566165db77423573ba57baebbe37439ddf
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 16:41:25 +1200

    test Joiner Should_Contain_TestWord_When_Given_Test_And_Word

---
+++ 7zPasswordCracker/Joiner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _7zPasswordCracker {
+    public class Joiner {
+        public IEnumerable<string> Join(List<string> words) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestProject1/TestJoiner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using _7zPasswordCracker;
+
+namespace Tests {
+    public class TestJoiner {
+        [Fact]
+        public void Should_Contain_TestWord_When_Given_Test_And_Word() {
+            var joiner = new Joiner();
+            var words = new List<string> { "test", "words" };
+
+            var results = joiner.Join(words);
+
+            Assert.Contains("testwords", results);
+        }
+    }
+}
