e10558bafa53894854738e08b7339cbe75194b11
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 16:42:55 +1200

    test Joiner Should_Contain_Test_Word_When_Given_Test_And_Word

--- UnitTestProject1/TestJoiner.cs
+++ UnitTestProject1/TestJoiner.cs
@@ -7,11 +7,21 @@ namespace Tests {
         [Fact]
         public void Should_Contain_TestWord_When_Given_Test_And_Word() {
             var joiner = new Joiner();
-            var words = new List<string> { "test", "words" };
+            var words = new List<string> { "test", "word" };
 
             var results = joiner.Join(words);
 
-            Assert.Contains("testwords", results);
+            Assert.Contains("testword", results);
+        }
+
+        [Fact]
+        public void Should_Contain_Test_Word_When_Given_Test_And_Word() {
+            var joiner = new Joiner();
+            var words = new List<string> { "test", "word" };
+
+            var results = joiner.Join(words);
+
+            Assert.Contains("test word", results);
         }
     }
 }
