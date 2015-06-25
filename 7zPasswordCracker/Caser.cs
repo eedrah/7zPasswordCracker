33988a42876b6d718a81e9748bc7b727d0160e80
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 17:49:04 +1200

    pass test

--- 7zPasswordCracker/Caser.cs
+++ 7zPasswordCracker/Caser.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace _7zPasswordCracker {
     public class Caser {
-        public IEnumerable<string> GetCases(string word)
-        {
-            yield return word;
+        public IEnumerable<string> GetCases(string word) {
+            ISet<string> cases = new HashSet<string> { word };
+            cases.Add(word.ToLower());
+            return cases;
         }
     }
 }
