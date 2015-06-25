3224d93c989255642e447cfd52e3b699b11c4557
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 17:02:39 +1200

    pass test Permutator Should_Return_Concatination_Of_Two_Words

--- 7zPasswordCracker/Permutator.cs
+++ 7zPasswordCracker/Permutator.cs
@@ -8,7 +8,12 @@ namespace _7zPasswordCracker {
     public class Permutator {
         public IEnumerable<string> Permutate(List<string> words)
         {
-            return words;
+            int numberOfWords = words.Count;
+            IEnumerable<List<int>> orderings = new OrdinalOrderer().GetOrderings(numberOfWords);
+            var joiner = new Joiner();
+            return orderings
+                .Select(order => order.Select(wordIndex => words[wordIndex]))
+                .SelectMany(candidateWords => joiner.Join(candidateWords));
         }
     }
 }
