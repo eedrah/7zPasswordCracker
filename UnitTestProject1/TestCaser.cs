73f6d5c380c04fd6a4c1e9f0d91ab23724684467
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 17:58:18 +1200

    test Caser Should_Return_Upper_Case_Of_Input_Word

--- UnitTestProject1/TestCaser.cs
+++ UnitTestProject1/TestCaser.cs
@@ -33,5 +33,15 @@ namespace Tests {
 
             Assert.Contains("Testing", results);
         }
+
+        [Fact]
+        public void Should_Return_Upper_Case_Of_Input_Word() {
+            var caser = new Caser();
+            var word = "teStIng";
+
+            IEnumerable<string> results = caser.GetCases(word);
+
+            Assert.Contains("TESTING", results);
+        }
     }
 }
