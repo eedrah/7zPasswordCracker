ac0824a823c567891913b671f6b60c2d00937f70
Eedrah <eedrah@users.noreply.github.com>
2015-06-25 16:41:55 +1200

    pass test

--- 7zPasswordCracker/Joiner.cs
+++ 7zPasswordCracker/Joiner.cs
@@ -3,8 +3,10 @@ using System.Linq;
 
 namespace _7zPasswordCracker {
     public class Joiner {
-        public IEnumerable<string> Join(List<string> words) {
-            throw new System.NotImplementedException();
+        public IEnumerable<string> Join(IEnumerable<string> words) {
+            IList<string> values = words as IList<string> ?? words.ToList();
+            yield return string.Join("", values);
+            yield return string.Join(" ", values);
         }
     }
 }
